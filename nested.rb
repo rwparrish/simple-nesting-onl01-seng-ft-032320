af153da19492a1fccfa9655cb30d2a5aa21eba8d
Ryan Parrish <rino3717@gmail.com>
2020-03-27 17:43:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,12 +59,12 @@ def dennis_ritchies_language
 end
 
 def adding_matz
- programmer_hash = 
-    {
-       :yukihiro_matsumoto => {
+    programmer_hash = 
+ 		{
+ 		    :yukihiro_matsumoto => {
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
-       },
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
