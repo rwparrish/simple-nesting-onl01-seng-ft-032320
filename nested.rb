8fcf2019db6b21f10be5a982dd9a5d780005abcf
Ryan Parrish <rino3717@gmail.com>
2020-03-27 17:45:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,25 +78,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     
 
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-    
 end
 
 def changing_alan
