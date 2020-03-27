8725b76cd7c38f94a66bf7e38f134aa98be9fd6f
Ryan Parrish <rino3717@gmail.com>
2020-03-27 17:53:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -97,7 +97,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for]  "GUI"
+     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = 
      
 end
 
