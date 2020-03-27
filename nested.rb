882d1ea343b5920f25251feb852a978d989700da
Ryan Parrish <rino3717@gmail.com>
2020-03-27 17:35:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,9 +61,10 @@ end
 def adding_matz
 # programmer_hash = 
 
-    {
-      
-    },
+    {   :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+       },
 
  		{
         :grace_hopper => {
